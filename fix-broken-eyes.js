b71e402b311adc73e01e9a985954b0e70342b61d
Zachary Williams <wil11063@byui.edu>
2018-04-18 11:58:10 -0600

    Corrected npm start and made some enhancements to the code

--- fix-broken-eyes.js
+++ fix-broken-eyes.js
@@ -6,6 +6,10 @@ const asyncLib = require('async');
 const prompt = require('prompt');
 var courseId;
 
+var imageSrc = 'Click to Define';
+var yellowEyeFilename = '_image_eyeButtonYellow.jpg';
+var yellowEyeFile;
+
 /* Starts the prompt which asks the user for the course Id and then runs getStuffFromCanvas */
 prompt.start();
 prompt.get('CourseId', (err, result) => {
@@ -13,12 +17,24 @@ prompt.get('CourseId', (err, result) => {
     getStuffFromCanvas();
 });
 
+/* Retrieves the ID for the yellow eye image */
+function getYellowEye(callback) {
+    canvas.get(`/api/v1/courses/${courseId}/files?search_term=${yellowEyeFilename}`, (err, files) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        yellowEyeFile = files[0];
+        callback(null);
+    });
+}
+
 /* Sends a request to Canvas to put the changed html into the course */
 function updatePage(page, callback) {
     var putObject = {
         'wiki_page[body]': page.$.html()
     };
-    canvas.put(`/api/v1/courses/${courseId}/pages/${page.url}`, putObject, (err) => {
+    canvas.put(`/api/v1/courses/${courseId}/pages/${page.url}`, putObject, (err, updatedPage) => {
         if (err) {
             console.log(err);
         }
@@ -28,42 +44,58 @@ function updatePage(page, callback) {
 
 /* Finds the src urls that have the wrong path and changes it to the correct path as well as adds the desired width of the src image */
 function fixEyes(page) {
-    page.$('img').each((i, elem) => {
-        if (elem.attribs.src && elem.attribs.src.includes('Eye%20button') && !elem.attribs.src.includes('SessionVal')) {
-            page.$(elem).attr('src', `https://byui.instructure.com/courses/${courseId}/file_contents/course%20files/template/gs120l_image_eyeButtonYellow.jpg`);
+    var changed = false;
+    page.$('img[alt="Click to Define"]').each((i, elem) => {
+        if (!elem.attribs.src.includes('SessionVal')) {
+            page.$(elem).attr('src', `/courses/${courseId}/files/${yellowEyeFile.id}/preview`);
             page.$(elem).attr('width', '4%');
+            changed = true;
+            console.log(`Page images fixed: ${page.name}`);
         }
     });
-    return page;
+    return changed;
+}
+
+/* Retrieves the full page, including its body */
+function getFullPage(page, callback) {
+    canvas.get(`/api/v1/courses/${courseId}/pages/${page.url}`, (err, fullPage) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fullPage = fullPage[0];
+            if (fullPage.body) {
+                fullPage.$ = cheerio.load(he.decode(fullPage.body));
+            }
+            callback(null, fullPage);
+        }
+    });
 }
 
 /* Sends a request to Canvas to get each page and their html. Calls fixEyes on each page to make desired changes */
 function getStuffFromCanvas() {
-    canvas.getPages(courseId, (err, pages) => {
-        asyncLib.mapLimit(pages, 30, (page, callback) => {
-            canvas.get(`/api/v1/courses/${courseId}/pages/${page.url}`, (err, fullPage) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    fullPage = fullPage[0];
-                    if (fullPage.body) {
-                        fullPage.$ = cheerio.load(he.decode(fullPage.body));
-                    }
-                    callback(null, fullPage);
-                }
-            });
-        }, (err, allPages) => {
-            if (err) {
-                console.log(err);
-            } else {
-                allPages = allPages.map(fixEyes);
-                asyncLib.eachLimit(allPages, 30, updatePage, (err) => {
+    getYellowEye((err) => {
+        if (err) {
+            console.log(err);
+        } else if (yellowEyeFile === undefined) {
+            console.log('The needed file does not exist in the course.');
+        } else {
+            console.log(`Needed image found: ${yellowEyeFile.display_name} | ${yellowEyeFile.id}`);
+            canvas.getPages(courseId, (err, pages) => {
+                asyncLib.mapLimit(pages, 30, getFullPage, (err, allPages) => {
                     if (err) {
                         console.log(err);
+                    } else {
+                        allPages = allPages.filter(fixEyes);
+                        console.log('Pages affected:', allPages.length);
+                        asyncLib.eachLimit(allPages, 30, updatePage, (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            console.log('Success!!');
+                        });
                     }
-                    console.log('Success!!');
                 });
-            }
-        });
+            });
+        }
     });
 }
